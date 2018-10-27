52e78f29889fed615c78536c0b91de55feaf5804
Nicholas Delgado <nickyd1218@Nicholass-MacBook-Pro-2.local>
2018-10-27 11:27:30 -0700

    added tweets

---
+++ Twitter/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "p0CRrugtuqFoArnGOVhpZb95Z"
+consumer_secret = "HLtWcy6arVqF8oXLa3SnXoyzDN5iwVmyWcarxUbVtudBW4ulIJ"
+access_token = "796277051950911488-JnpALB5nGZatFmsYHwhAGGWivduwCT1"
+access_token_secret = "2EEZPEe2qsq57QvGxpIxVIIe5XqfeosFC04YtdjtHaplR"
